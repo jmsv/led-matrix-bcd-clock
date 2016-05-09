849f026809ee55140acb4f1ed5d2a9774e3558cc
James Vickery <jamesevickery@users.noreply.github.com>
2016-05-09 18:37:55 +0100

    Found a more accurate/reliable way of keeping the matrix updates in sync with the time (much better than the last attempt)

--- bcd-clock.py
+++ bcd-clock.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import max7219.led as led
-from time import sleep
+from time import time, sleep
 import datetime, thread
 
 matrix = led.matrix(cascaded=1)
@@ -11,35 +11,30 @@ def main():
     prev_sec = datetime.datetime.now().second
     while(datetime.datetime.now().second == prev_sec):
         pass
-    sleep(0.4)
+    sleep(0.005)
+    starttime = time()
     while(True):
-        thread.start_new_thread(clock_update, ())
-        sleep(0.99871)
-        # ^ sleep of just less than a second
-        # ^ actual value based on experimental data
-
-def clock_update():
-    now = datetime.datetime.now()
-    display_number(now.hour, now.minute, now.second)
+        now = datetime.datetime.now()
+        display_number(now.hour, now.minute, now.second)
+        #print('time:  ' + str(time())
+        #      + '    start time:  ' + str(starttime)
+        #      + '    current microseconds:  ' + str(now.microsecond))
+        '''Uncomment 3 lines above to print time stats'''
+        sleep(1.0 - ((time() - starttime) % 1.0))
 
 
 def display_number(hours, minutes, seconds):
     bin_list = []
-    #hours
+    '''hours'''
     stringh = str(hours).zfill(2)
-    bin_list.append(den_to_bin(stringh[0]).zfill(4))    
-    bin_list.append(den_to_bin(stringh[1]).zfill(4))
-    #minutes
+    bin_list.extend(den_to_bin(stringh[n]) for n in range(0, 2))
+    '''minutes'''
     stringm = str(minutes).zfill(2)
-    bin_list.append(den_to_bin(stringm[0]).zfill(4))    
-    bin_list.append(den_to_bin(stringm[1]).zfill(4))
-    #seconds
+    bin_list.extend(den_to_bin(stringm[n]) for n in range(0, 2))
+    '''seconds'''
     strings = str(seconds).zfill(2)
-    bin_list.append(den_to_bin(strings[0]).zfill(4))    
-    bin_list.append(den_to_bin(strings[1]).zfill(4))
-    #matrix
+    bin_list.extend(den_to_bin(strings[n]) for n in range(0, 2))
     matrix_led(bin_list)
-    #print(bin_list)
     del bin_list[:]
 
 
@@ -50,10 +45,11 @@ def matrix_led(binary_list):
             
 
 def den_to_bin(n):
-    return '{0:04b}'.format(int(n))
+    return '{0:04b}'.format(int(n)).zfill(4)
 
 
 #main() #uncomment when debugging
+
 
 try:
     main()
@@ -64,5 +60,4 @@ except:
         print('Done. Exiting...')
     except:
         print('\nAborting...')
-
 
